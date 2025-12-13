ab4694f9356ad33374d1f93870e71b555a54ebdb
Laura <laurabejaranoa22@gmail.com>
2025-12-13 11:43:48 +0100

    4.md

--- questions-and-reviews/questions/1.js
+++ questions-and-reviews/questions/1.js
@@ -2,7 +2,7 @@
 // Remember to write your prediction first, before you run this file
 
 const currentYear = 2023;
-currentYear();
+currentYear(); // is a variable no a function
 
 // const currentYear will  print UNDEFINE because it is not a function
 // and currentYear() is not a function call so it will give an error
